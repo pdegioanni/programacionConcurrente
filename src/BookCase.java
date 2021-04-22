264a6b7080d88fbaa983b843a253b51b2f0e6201
PaulaDegioanni <32582829+PaulaDegioanni@users.noreply.github.com>
2021-04-22 17:47:26 -0300

    se escribe el código de todas las clases

---
+++ src/BookCase.java
@@ -0,0 +1,88 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class BookCase {
+    //PRIVATES VARIABLES
+    //------------------------------------------------------------------------------------------------------------------
+    private ArrayList<Book> allBooks;
+    private final int  numberOfBooks;
+    private final int numberOfWriters;
+    private final int numberOfReaders;
+
+    //CONSTRUCTOR
+    //------------------------------------------------------------------------------------------------------------------
+    public BookCase(int numberOfBooks, int numberOfWriters, int numberOfReaders){
+        allBooks = new ArrayList<>();
+        this.numberOfBooks = numberOfBooks;
+        this.numberOfWriters = numberOfWriters;
+        this.numberOfReaders = numberOfReaders;
+        for (int i=0; i<numberOfBooks; i++){
+            allBooks.add(new Book(i, numberOfWriters, numberOfReaders));
+        }
+    }
+    //PUBLIC METHODS
+    //------------------------------------------------------------------------------------------------------------------
+    public Book getBook(ArrayList<Book> notValidBooks){
+        Random rand = new Random();
+        Book bookSelected;
+        /*try {
+            bookSelected = allBooks.stream().
+                                filter(book -> notValidBooks.contains(book) == false).
+                                findFirst().get();
+        //} catch (NoSuchElementException e){
+         //   e.printStackTrace();
+       // }*/
+
+        List<Book> validBooks = allBooks.stream().
+                            filter(book -> !notValidBooks.contains(book)).toList();
+        bookSelected = validBooks.get(rand.nextInt(validBooks.size()));
+        return bookSelected;
+    }
+
+    public ArrayList<Book> getAllBooks() {
+        return allBooks;
+    }
+
+    public boolean checkAllBooksReady(){
+        boolean allBooksReady = false;
+        long booksReady = allBooks.stream().filter(Book::isReady).count();
+        if(booksReady == numberOfBooks){
+            allBooksReady = true;
+        }
+        return allBooksReady;
+    }
+
+    public String getBooksStats(){
+        StringBuilder result = new StringBuilder();
+        for(Book book : allBooks){
+            result.append("Book ").append(book.getIdNumber()).append(": has ").append(book.getReviews()).append(" reviews and ").append(book.getReads()).append(" reads\n");
+        }
+        return result.toString();
+    }
+
+    public int getAmountOfBooksInFinalVersion(){
+        int booksInFinalVersion = 0;
+        for (Book book: allBooks){
+            if(book.isFinalVersion()){
+                booksInFinalVersion++;
+            }
+        }
+        return booksInFinalVersion;
+    }
+
+    public int getAmountOfBooksReady(){
+        int booksReady = 0;
+        for(Book book: allBooks){
+            if(book.isReady()) {
+                booksReady++;
+            }
+        }
+        return booksReady;
+    }
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
+}
